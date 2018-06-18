81b29bd23c98c5fdfce729d80a0421d49e9b709d
abdulrahman alaa <abdelraman.bashir@vodafone.com>
2018-06-18 05:21:38 +0200

    adding toggle menu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,17 +4,24 @@ import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 class Layout extends Component {
-    state={
-        showSideDrawer:true
+    state = {
+        showSideDrawer: false
     };
-    closeSideDrawerHandler = ()=>{
-        this.setState({showSideDrawer:false});
+    closeSideDrawerHandler = () => {
+        this.setState({ showSideDrawer: false });
     }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState)=>({ showSideDrawer: !prevState.showSideDrawer }));
+    }
+    
     render() {
         return (
             <Aux>
-                <Toolbar />
-                <SideDrawer open={this.state.showSideDrawer} close={this.closeSideDrawerHandler} />
+                <Toolbar  drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    close={this.closeSideDrawerHandler} />
                 <main className={classes.Content}>
                     {this.props.children}
                 </main>
